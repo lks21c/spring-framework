8bef5df6c973a7e091c927932a4c428312e87ddc
kwangsik lee <lks21c@gmail.com>
2014-12-25 19:14:37 +0900

    org.springframework.mock.env 패키지 한글화

--- spring-test/src/main/java/org/springframework/mock/env/MockPropertySource.java
+++ spring-test/src/main/java/org/springframework/mock/env/MockPropertySource.java
@@ -22,17 +22,10 @@ import org.springframework.core.env.PropertiesPropertySource;
 import org.springframework.core.env.PropertySource;
 
 /**
- * Simple {@link PropertySource} implementation for use in testing. Accepts
- * a user-provided {@link Properties} object, or if omitted during construction,
- * the implementation will initialize its own.
- *
- * The {@link #setProperty} and {@link #withProperty} methods are exposed for
- * convenience, for example:
- * <pre class="code">
- * {@code
- *   PropertySource<?> source = new MockPropertySource().withProperty("foo", "bar");
- * }
- * </pre>
+ * 유닛 테스트를 위한 간단한 {@link PropertySource} 구현. <br />
+ * 사용자가 지정한 프로퍼티 이름과 프로퍼티를 사용하거나, <br />
+ * {@link MOCK_PROPERTIES_PROPERTY_SOURCE_NAME} 이름으로 프로퍼티를 생성 할 수 있음. <br />
+ * 
  *
  * @author Chris Beams
  * @since 3.1
@@ -41,60 +34,67 @@ import org.springframework.core.env.PropertySource;
 public class MockPropertySource extends PropertiesPropertySource {
 
 	/**
-	 * {@value} is the default name for {@link MockPropertySource} instances not
-	 * otherwise given an explicit name.
+	 * 이 값{@value} 은 생성자에서 이름을 정해주지 않을때 쓰는 {@link MockPropertySource}의 기본 이름
+	 *
 	 * @see #MockPropertySource()
 	 * @see #MockPropertySource(String)
 	 */
 	public static final String MOCK_PROPERTIES_PROPERTY_SOURCE_NAME = "mockProperties";
 
 	/**
-	 * Create a new {@code MockPropertySource} named {@value #MOCK_PROPERTIES_PROPERTY_SOURCE_NAME}
-	 * that will maintain its own internal {@link Properties} instance.
+	 * {@code MockPropertySource} 생성자 <br />
+	 * 프로퍼티 이름 : {@value #MOCK_PROPERTIES_PROPERTY_SOURCE_NAME}로 지정 <br />
+	 * 프로퍼티 : 프로퍼티는 내부에서 인스턴스 생성.
 	 */
 	public MockPropertySource() {
 		this(new Properties());
 	}
 
 	/**
-	 * Create a new {@code MockPropertySource} with the given name that will
-	 * maintain its own internal {@link Properties} instance.
-	 * @param name the {@linkplain #getName() name} of the property source
+	 * {@code MockPropertySource} 생성자 <br />
+	 * 프로퍼티 이름 : 사용할 프로퍼티 이름을 파라미터로 지정 <br />
+	 * 프로퍼티 : 프로퍼티는 내부에서 인스턴스 생성.
+	 * 
+	 * @param 프로퍼티 소스의 이름을 지정
 	 */
 	public MockPropertySource(String name) {
 		this(name, new Properties());
 	}
 
 	/**
-	 * Create a new {@code MockPropertySource} named {@value #MOCK_PROPERTIES_PROPERTY_SOURCE_NAME}
-	 * and backed by the given {@link Properties} object.
-	 * @param properties the properties to use
+	 * {@code MockPropertySource} 생성자 <br />
+	 * 프로퍼티 이름 : {@value #MOCK_PROPERTIES_PROPERTY_SOURCE_NAME}로 지정 <br />
+	 * 프로퍼티 : 사용할 프로퍼티를 파라미터로 지정
+	 * 
+	 * @param {@link MockPropertySource}에서 사용한 Properties를 지정
 	 */
 	public MockPropertySource(Properties properties) {
 		this(MOCK_PROPERTIES_PROPERTY_SOURCE_NAME, properties);
 	}
 
 	/**
-	 * Create a new {@code MockPropertySource} with the given name and backed by the given
-	 * {@link Properties} object.
-	 * @param name the {@linkplain #getName() name} of the property source
-	 * @param properties the properties to use
+	 * {@code MockPropertySource} 생성자 <br />
+	 * 프로퍼티 이름 : 사용할 프로퍼티 이름을 파라미터로 지정 <br />
+	 * 프로퍼티 : 사용할 프로퍼티를 파라미터로 지정
+	 * 
+	 * @param 사용할 프로퍼티 이름
+	 * @param 사용할 프로퍼티
 	 */
 	public MockPropertySource(String name, Properties properties) {
 		super(name, properties);
 	}
 
 	/**
-	 * Set the given property on the underlying {@link Properties} object.
+	 * {@link Properties}에 프로퍼티를 set함.
 	 */
 	public void setProperty(String name, Object value) {
 		this.source.put(name, value);
 	}
 
 	/**
-	 * Convenient synonym for {@link #setProperty} that returns the current instance.
-	 * Useful for method chaining and fluent-style use.
-	 * @return this {@link MockPropertySource} instance
+	 * setProperty를 fluent-style로 사용할 때 활용.
+	 * 
+	 * @return {@link MockPropertySource} 인스턴스
 	 */
 	public MockPropertySource withProperty(String name, Object value) {
 		this.setProperty(name, value);
--- spring-test/src/main/java/org/springframework/mock/env/package-info.java
+++ spring-test/src/main/java/org/springframework/mock/env/package-info.java
@@ -1,10 +1,11 @@
 /**
- * This package contains mock implementations of the
- * {@link org.springframework.core.env.Environment Environment} and
+ * 이 패키지는 mock 구현을 포함합니다.
+ * 스프링 3.1에서 소개된 {@link org.springframework.core.env.Environment Environment} 와
  * {@link org.springframework.core.env.PropertySource PropertySource}
- * abstractions introduced in Spring 3.1.
+ * 를 mock으로 감싸서 테스트 할 수 있습니다.
  *
- * <p>These <em>mocks</em> are useful for developing <em>out-of-container</em>
- * unit tests for code that depends on environment-specific properties.
+ * 이 mock 구현체들은 컨테이너 밖에서 environment 프로퍼티에 의존하는 유닛 테스트를  작성하는데 유용합니다.
  */
+
 package org.springframework.mock.env;
+
