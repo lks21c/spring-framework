795075a142824b27eda3cb2a5528dabfbfca3467
kwangsik lee <lks21c@gmail.com>
2014-12-25 18:49:48 +0900

    Eclipse Formatting

--- spring-test/src/main/java/org/springframework/mock/env/MockEnvironment.java
+++ spring-test/src/main/java/org/springframework/mock/env/MockEnvironment.java
@@ -20,7 +20,7 @@ import org.springframework.core.env.AbstractEnvironment;
 import org.springframework.core.env.ConfigurableEnvironment;
 
 /**
-	 * 간단한 {@link ConfigurableEnvironment}의 구현으로 {@link #setProperty(String, String)} 와
+ * 간단한 {@link ConfigurableEnvironment}의 구현으로 {@link #setProperty(String, String)} 와
  * {@link #withProperty(String, String)} 메서드를 테스트 목적으로 제공한다.
  *
  * @author Chris Beams
@@ -33,7 +33,7 @@ public class MockEnvironment extends AbstractEnvironment {
 	private MockPropertySource propertySource = new MockPropertySource();
 
 	/**
-        * 하나의 {@link MockPropertySource}를 가진 {@code MockEnvironment}를 생성함.
+	 * 하나의 {@link MockPropertySource}를 가진 {@code MockEnvironment}를 생성함.
 	 */
 	public MockEnvironment() {
 		getPropertySources().addLast(propertySource);
@@ -47,8 +47,9 @@ public class MockEnvironment extends AbstractEnvironment {
 	}
 
 	/**
-	 * 편리하게 {@link #setProperty} 대신 쓸수 있는 메서드.
-	 * fluent-style(역자주 : Build Up 패턴)으로 프로퍼티를 set할때 사용하기 편리함.
+	 * 편리하게 {@link #setProperty} 대신 쓸수 있는 메서드. fluent-style(역자주 : Build Up 패턴)으로 프로퍼티를
+	 * set할때 사용하기 편리함.
+	 * 
 	 * @return 현재 {@link MockEnvironment} 인스턴스를 리턴함
 	 * @see MockPropertySource#withProperty
 	 */
